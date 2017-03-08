0dc7e3d3caf6588d32a7bc97b3c3654426a3bf31
unknown <k90218104@gcloud.csu.edu.tw>
2017-03-08 16:47:12 +0800

    commit connect/connect.php

--- connect/connect.php
+++ connect/connect.php
@@ -13,9 +13,38 @@
   }
 
   Class Sql{
-    //查詢
-    function Selects(){
 
+    //宣告post變數
+    function Variables($db, $Button){//$Button存入的是新增或刪除或查詢的值
+      $dbs = $db;//連線
+      $Status = $Button;//存入狀態值
+      // $Shelf_Number = $_POST["Shelf_Number"];//架號
+      // $Journal = $_POST["Journal"];//刊名
+      // $Classification = $_POST["Classification"];//刊別
+      // $Language = $_POST["Language"];//語言
+      // $Budget = $_POST["Budget"];//預算科別
+      // $Money = $_POST["Money"];//金額
+      // $Source = $_POST["Source"];//來源
+
+      switch ($Status) {
+        case 'Insert':
+        // return Selects($db);
+         $this->Selects($dbs);
+          break;
+
+        default:
+          # code...
+          break;
+      }
+    }
+    //宣告post變數
+
+    //查詢
+    function Selects($db){
+      $SeSql = "SELECT * FROM `booklist` WHERE 1";
+      $SeQu = $db->query($SeSql);
+      $SeDis = $SeQu->fetchAll();
+      return $SeDis;
     }
     //查詢
 
